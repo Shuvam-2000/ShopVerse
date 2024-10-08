a5c00d539fc88e59870ed3d7a7506316d56bf5e2
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-10-08 08:42:00 +0530

    all the nessecary comoponents, store and app component inside the src folder

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-2xl text-black tracking-widest bg-red-300'>
+        <h1>Hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
