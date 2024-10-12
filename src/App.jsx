0dffc3c10018962dc9c60c04184b0bebd4357e8a
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-10-12 16:39:44 +0530

    Home page , navabr and footer component added to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import './index.css'
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import './index.css';
 
 function App() {
-
   return (
-    <>
-      <div className='text-2xl text-black tracking-widest bg-red-300'>
-        <h1>Hello</h1>
+      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Footer/>
+        </BrowserRouter>
       </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
