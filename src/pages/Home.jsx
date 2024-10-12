2ba04b0e450b152166ff0272776377ba2cdcfeca
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-10-12 16:40:31 +0530

    home page created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+import Newsletter from "../components/Newsletter";
+import TopProducts from "../components/TopProducts";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <TopProducts />
+      <Newsletter />
+    </div>
+  );
+};
+
+export default Home;
