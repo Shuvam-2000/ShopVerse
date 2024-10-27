1336e0e994b663024e745c81b88e263528eb5414
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-10-27 16:11:59 +0530

    componets and the routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import ContactUs from './pages/ContactUs';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import ExploreProducts from './pages/ExploreProducts';
+import AboutUs from './pages/AboutUs';
+import SignIn from './pages/SignIn';
+import Cart from './pages/Cart';
+import Products from './pages/Products';
+import PlaceOrder from './pages/PlaceOrder';
+import OurPolicies from './components/OurPolicies';
+import ScrollOnTop from './components/ScrollOnTop';
+import SearchBar from './components/SearchBar';
 import './index.css';
 
 function App() {
   return (
       <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
         <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-          <Footer/>
+          <ScrollOnTop/>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contact" element={<ContactUs />} />
+              <Route path="/exploreproducts" element={<ExploreProducts />} />
+              <Route path="/aboutus" element={<AboutUs />} />
+              <Route path="/login" element={<SignIn />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/product/:productId" element={<Products />} />
+              <Route path="/placeorder" element={<PlaceOrder />} />
+              <Route path="/ourpolicies" element={<OurPolicies />} />
+              <Route path="/searchbar" element={<SearchBar />} />
+            </Routes>
+            <Footer/>
         </BrowserRouter>
       </div>
   );
