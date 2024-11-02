1de26e92687968a9e86d3334e6af7d46b6f86bff
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-11-02 16:41:19 +0530

    hero section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,29 @@
+import hero_img from "../assets/hero.jpg"
 
 const Hero = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default Hero
+    return (
+      <div className="flex flex-col sm:flex-row border border-gray-300 rounded-md sm:font-mono font-serif"> 
+        {/* Hero left side */}
+        <div className="flex flex-col items-center justify-center w-full sm:w-1/2 py-10 sm:py-0 text-center sm:text-left bg-slate-100">
+          <div className="text-[#414141]">
+            <div className="flex items-center gap-2">
+              <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+              <p className="font-medium text-xs md:text-base">Shop Smarter. <span className="text-[#f21c1c]">Live Better.</span></p>
+              <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
+            </div>
+            <h1 className="text-2xl sm:py-3 lg-text-4xl leading-relaxed">Our <span className="text-[#f21c1c]">Latest Collections</span></h1>
+            <div className="flex items-center gap-2">
+                <p className="w-8 md:w-11 h-[2px] bg-[#f21c1c]"></p>
+                <p className="font-semibold text-xs md:text-base">Shop Now</p>
+                <p className="w-8 md:w-11 h-[2px] bg-[#f21c1c]"></p>
+            </div>
+          </div>
+        </div>
+        {/* Hero right side */}
+        <img src={hero_img} alt="hero-image" className="w-full sm:w-1/2 object-cover"/>
+      </div>
+    );
+  }
+  
+  export default Hero;
+  
