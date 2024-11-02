0f5748f4be80f6b32ec5087038c40452e59a3320
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-11-02 16:42:52 +0530

    TopProducts component added

--- src/components/TopProducts.jsx
+++ src/components/TopProducts.jsx
@@ -1,10 +1,55 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const TopProducts = () => {
+
+  // state for rendering top product data from API in the home page
+  const [topproducts, setTopProducts] = useState([]);
+
+  // Fetching data from API
+  useEffect(() => {
+    const fetchData = async () => {
+        try{
+            const res = await axios.get('https://fakestoreapi.com/products')
+            setTopProducts(res.data.slice(0,8))
+        }catch(error){
+            console.error("Items Not Found", error)
+        }
+    }
+    fetchData()
+    },[])
+
   return (
-    <div>
-      
+    <div className="my-10">
+      {/* Top Products Title Section */}
+      <div className="text-center py-8 sm:text-3xl text-2xl">
+        <div className="inline-flex gap-2 items-center mb-3">
+          <p className="text-[#414141]">Our Top <span className="text-[#f21c1c]">Collection</span></p>
+          <p className="w-8 sm:w-12 h-[2px] bg-gray-700"></p>
+        </div>
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-[#414141]">
+          Have a look at our newest additions and trends. Our latest collection caters to all your needs.
+        </p>
+      </div>
+
+      {/* Top Products On the Home Page */}
+      <div className='grid grid-cols-1 sm:gird-cols-2 md:gird-cols-3 lg:grid-cols-4 gap-4 sm:gap-8'>
+      {topproducts.map((showTopProduct) => (
+        <div key={showTopProduct.id} className='overflow-hidden rounded-lg shadow sm:hover:scale-110 duration-500 transition ease-in-out'>
+          <Link to={`/product/${showTopProduct.id}`} className='w-full h-40 sm:mb-5 px-2 rounded-lg'>
+          <img src={showTopProduct.image} alt={showTopProduct.title} className="sm:w-full w-[90%] h-40 sm:mb-5 px-2 rounded-lg" />
+          <p className="sm:text-sm text-xs font-serif mt-2 ml-2 sm:font-mono text-[#414141]">{showTopProduct.title}</p>
+          <p className="text-[#414141] font-bold mt-4 ml-2">Rs. {showTopProduct.price}</p>
+          </Link>
+        </div>
+      ))}
+      </div>
+
     </div>
   )
 }
 
-export default TopProducts
+export default TopProducts;
+
+
