bb88ef2b6a1e5bfe8edfca6f6321e7bd64da0702
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-11-08 07:46:32 +0530

    best seller component completed on home page

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -22,10 +22,10 @@ const TopProducts = () => {
 
   return (
     <div className="my-10">
-      {/* Top Products Title Section */}
+      {/* Best Selling Products Title Section */}
       <div className="text-center py-8 sm:text-3xl text-2xl">
         <div className="inline-flex gap-2 items-center mb-3">
-          <p className="text-[#414141]">Our Best <span className="text-[#f21c1c]">Sellers</span></p>
+          <p className="text-[#414141]">Our Top <span className="text-[#f21c1c]">Sellers</span></p>
           <p className="w-8 sm:w-12 h-[2px] bg-gray-700"></p>
         </div>
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-[#414141]">
@@ -33,14 +33,13 @@ const TopProducts = () => {
         </p>
       </div>
 
-      {/* Top Products On the Home Page */}
+      {/* Best Selling Products On the Home Page */}
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-8'>
       {bestproducts.map((showBestProduct) => (
-        <div key={showBestProduct.id} className='overflow-hidden rounded-lg shadow sm:hover:scale-110 duration-500 transition ease-in-out'>
+        <div key={showBestProduct.id} className='overflow-hidden rounded-lg shadow-lg sm:hover:shadow-2xl transition-all duration-300 hover:bg-gray-100 '>
           <Link to={`/product/${showBestProduct.id}`} className='w-full h-40 sm:mb-5 px-2 rounded-lg'>
           <img src={showBestProduct.image} alt={showBestProduct.title} className="sm:w-full w-[90%] h-40 sm:mb-5 px-2 rounded-lg" />
-          <p className="sm:text-sm text-xs font-serif mt-2 ml-2 sm:font-mono text-[#414141]">{showBestProduct.title}</p>
-          <p className="text-[#414141] font-bold mt-4 ml-2">Rs. {showBestProduct.price}</p>
+          <p className="sm:text-sm text-xs font-serif mt-2 ml-2 sm:font-mono text-[#414141] text-center">{showBestProduct.title}</p>
           </Link>
         </div>
       ))}
