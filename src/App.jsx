90e5e86937ca1163abf9e17b768def299f5e7537
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-11-08 07:46:03 +0530

    fixed the padding for large screen

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import './index.css';
 
 function App() {
   return (
-      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[6vw]'>
         <BrowserRouter>
           <ScrollOnTop/>
             <Navbar />
