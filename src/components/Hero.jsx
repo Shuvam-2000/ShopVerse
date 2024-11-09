0f2f788078183efb483984386c54cf3065dce3c2
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-11-09 16:49:11 +0530

    fixed few error in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
           <div className="text-[#414141]">
             <div className="flex items-center gap-2">
               <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
-              <p className="font-medium text-xs md:text-base">Shop Smarter. <span className="text-[#f21c1c]">Live Better.</span></p>
+              <p className="font-medium text-xs md:text-base">Smart Shopping. <span className="text-[#f21c1c]">Better Living.</span></p>
               <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
             </div>
             <h1 className="text-2xl sm:py-3 lg-text-4xl leading-relaxed">Our <span className="text-[#f21c1c]">Latest Collections</span></h1>
