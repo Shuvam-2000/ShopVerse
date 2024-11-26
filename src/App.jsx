78b70029d9db10608726bacee80db832b556d07c
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-11-26 16:37:44 +0530

    Update: updtaed the app component with the frontend form validations fucntionalites

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { useForm } from 'react-hook-form'
 import Navbar from './components/Navbar';
 import ContactUs from './pages/ContactUs';
 import Home from './pages/Home';
@@ -17,6 +18,17 @@ import ForgotPassoword from './pages/ForgotPassoword';
 import './index.css';
 
 function App() {
+  const {
+    register,
+    // setValue,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Form Submitted", data)
+  }
+
   return (
       <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[4vw]'>
         <BrowserRouter>
@@ -27,8 +39,20 @@ function App() {
               <Route path="/contact" element={<ContactUs />} />
               <Route path="/exploreproducts" element={<ExploreProducts />} />
               <Route path="/aboutus" element={<AboutUs />} />
-              <Route path="/login" element={<SignIn />} />
-              <Route path="/signup" element={<SignUp />} />
+              <Route 
+                path="/login" 
+                register={register} 
+                handleSubmit={handleSubmit} 
+                errors={errors} 
+                onSubmit={onSubmit} 
+                isSubmitting={isSubmitting}  element={<SignIn />} />
+
+              <Route path="/signup" 
+                register={register} 
+                handleSubmit={handleSubmit} 
+                errors={errors} onSubmit={onSubmit} 
+                isSubmitting={isSubmitting}  element={<SignUp />} />
+                
               <Route path="/forgotpassowrd" element={<ForgotPassoword />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/product/:productId" element={<Products />} />
